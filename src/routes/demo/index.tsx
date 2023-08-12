ee1e6771dfef4aed427bb2d7837a1e53f1040550
Basil <basil.dimopoulos@dewc.com>
2023-08-12 11:32:11 +0930

    responsivness

--- src/routes/demo/index.tsx
+++ src/routes/demo/index.tsx
@@ -8,8 +8,8 @@ export default component$(() => {
 
   return (
     <div>
-      <div class="w-full h-screen xl:pl-44 xl:pr-44 pl-20 pr-5 pt-12 pb-16 bg-[#010715] flex flex-col justify-center">
-        <div class="flex flex-col items-center pt-14">
+      <div class="w-full h-screen xl:pl-44 xl:pr-44 pl-12 pr-5 pt-12 pb-16 bg-[#010715] flex flex-col justify-center">
+        <div class="flex flex-col items-center lg:pt-14 pt-5">
           <div class="lg:w-3/6 md:5/6 w-4/6  bg-gray-200 h-2 dark:bg-gray-700 rounded-full">
             <div
               style={{ width: `${((activePage.value + 2) / 4) * 100}%` }}
